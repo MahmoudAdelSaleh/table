4276b0bb1973732aed4aa8fae3fd476f7c03e12f
MahmoudAdelSaleh <mahmoudadelsaleh@outlook.com>
2025-09-17 00:43:39 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,13 @@
 // ملف: firebase-config.js
 const firebaseConfig = {
-    apiKey: "AIzaSyCcEY4xNGP0LNaUTsSIEL9bALstG1736Xc",
-    authDomain: "table-bff6c.firebaseapp.com",
-    projectId: "table-bff6c",
-    storageBucket: "table-bff6c.appspot.com",
-    messagingSenderId: "1003725939997",
-    appId: "1:1003725939997:web:f268b159abda867e2dedf4",
-    measurementId: "G-7FXX00EL6V"
+  apiKey: "AIzaSyCJO-P9t196m4lzBPp7EHjQLUhWikIMH3U",
+  authDomain: "turing-diode-468718-b3.firebaseapp.com",
+  databaseURL: "https://turing-diode-468718-b3-default-rtdb.firebaseio.com",
+  projectId: "turing-diode-468718-b3",
+  storageBucket: "turing-diode-468718-b3.firebasestorage.app",
+  messagingSenderId: "492366336701",
+  appId: "1:492366336701:web:e56c66aaff89c2af709dd4",
+  measurementId: "G-FPRZJRNG29"
 };
-
 // تهيئة خدمات Firebase
 firebase.initializeApp(firebaseConfig);
