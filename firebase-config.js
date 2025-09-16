3e1395a14f8b8befe9224dd54a20320edf843a6c
MahmoudAdelSaleh <mahmoudadelsaleh@outlook.com>
2025-09-16 13:49:24 +0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,16 @@
+// ملف: firebase-config.js
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCcEY4xNGP0LNaUTsSIEL9bALstG1736Xc",
+  authDomain: "table-bff6c.firebaseapp.com",
+  projectId: "table-bff6c",
+  storageBucket: "table-bff6c.firebasestorage.app",
+  messagingSenderId: "1003725939997",
+  appId: "1:1003725939997:web:f268b159abda867e2dedf4",
+  measurementId: "G-7FXX00EL6V"
+};
+
+// تهيئة خدمات Firebase
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
