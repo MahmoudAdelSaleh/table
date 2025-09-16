93995d99318cdff6862f7890108987ca04f94f67
MahmoudAdelSaleh <mahmoudadelsaleh@outlook.com>
2025-09-16 16:00:55 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,16 +1,13 @@
 // ملف: firebase-config.js
-// !!! استبدل هذا الكود بالبيانات الخاصة بك من مشروع Firebase !!!
 const firebaseConfig = {
-  apiKey: "AIzaSyCcEY4xNGP0LNaUTsSIEL9bALstG1736Xc",
-  authDomain: "table-bff6c.firebaseapp.com",
-  projectId: "table-bff6c",
-  storageBucket: "table-bff6c.firebasestorage.app",
-  messagingSenderId: "1003725939997",
-  appId: "1:1003725939997:web:f268b159abda867e2dedf4",
-  measurementId: "G-7FXX00EL6V"
+    apiKey: "AIzaSyCcEY4xNGP0LNaUTsSIEL9bALstG1736Xc",
+    authDomain: "table-bff6c.firebaseapp.com",
+    projectId: "table-bff6c",
+    storageBucket: "table-bff6c.appspot.com",
+    messagingSenderId: "1003725939997",
+    appId: "1:1003725939997:web:f268b159abda867e2dedf4",
+    measurementId: "G-7FXX00EL6V"
 };
 
 // تهيئة خدمات Firebase
 firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
-const db = firebase.firestore();
